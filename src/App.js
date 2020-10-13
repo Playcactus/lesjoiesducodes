27f3a7bf3ed1789857c4862c17d4cb2a9b84c387
playcactus <playcactusenvo@mail.com>
2020-10-13 17:32:57 +0200

    <au milieu du projet>

--- src/App.js
+++ src/App.js
@@ -34,12 +34,20 @@ function App() {
   </div>
 </nav>
   <div class="row">
-    <div class="col-6 bloque1">
+    <div class="bloque1 border border-gray">
  
-       <p class="title"> Quand je découvre combien je suis facturé au client</p>
-       
+       <p class="text1"> Quand je découvre combien je suis facturé au client</p>
+       <video autoplay="true" loop class ="video"  src="https://lesjoiesducode.fr/content/042/v8JLf19.webm" ></video>
+          <div class="container">
+            <div class="row">
+                  <div class="col"><button type="button" class="btn btn-primary">Facebook</button></div>
+                  <div class="col"><button type="button" class="btn btn-info">Twitter</button></div>
+                  <div class="col"><button type="button" class="btn btn-secondary">E-mail</button></div>
+                  <div class="col">col</div>
+            </div>
+          </div>
     </div>
-    <div class="col-3 bloque1">
+    <div class="bloque2">
       One of two columns
     </div>
   </div>
